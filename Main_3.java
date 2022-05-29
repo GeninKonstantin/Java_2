27d0ca8af6403839488f9974028b821303cf0a52
GeninKonstantin <91433238+GeninKonstantin@users.noreply.github.com>
2022-05-29 14:47:47 +0300

    Create Main_3.java

---
+++ Main_3.java
@@ -0,0 +1,36 @@
+package Java2;
+import java.util.Random;
+
+public class java3 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[4][4];
+        int[] vectorB = new int[4];
+        System.out.println("\nМатрица:");
+        for (int i = 0; i < matrix.length; i++) {
+            int count = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                int element = -7 + new Random().nextInt(17);
+                matrix[i][j] = element;
+                if (element % 2 == 0) {
+                    count++;
+                }
+            }
+            vectorB[i] = count;
+        }
+
+        for (int[] matrixItem : matrix) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrixItem[j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+
+        for (int j : vectorB) {
+            System.out.print("\nКоличество четных чисел в i-й строке: " + j);
+        }
+
+    }
+}
+/*Определить матрицу (двумерный массив) и ее заполнить случайными значени- ями.
+Построить вектор В, которой возвращает – количество четных чисел в i-й строке.*/
